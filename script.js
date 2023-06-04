dd6323345c38a8f419bb813a39b7fdfa84d0b0d1
ArriusDM <ArriusDM@users.noreply.github.com>
2023-06-04 15:37:49 -0700

    Implement box filling and temporary SVG background

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ console.log(container)
 
 
 
+
 // Add a prompt for user size input
-let userSize = 100
+let userSize = 12
 
 
 
@@ -14,12 +15,21 @@ function rowsAndColumns(numBoxes) {
 
         let calc1 = (100 / numBoxes) + "%";
         let calc2 = (100 / numBoxes) + "%";
-        
+
         console.log(calc1, calc2)
-        box.setAttribute('class', 'grid4')
-        box.setAttribute('style',`height:${calc1};width:${calc2}`)
+
+        box.setAttribute('style', `height:${calc1};width:${calc2}`)
         container.appendChild(box)
-    }
+        
+        box.addEventListener('mousedown', e => {
+            box.setAttribute('class', 'filled')
+        })
+
+    } 
+
 }
+
+
+
 
 rowsAndColumns(userSize)
