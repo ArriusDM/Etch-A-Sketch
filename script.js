1e6b4a6efd0c0db6947484b830aaf5cb3f76d4a8
ArriusDM <ArriusDM@users.noreply.github.com>
2023-06-13 17:54:08 -0700

    Implement grid scaling and drawing styles.
    Also cleanup unused code

--- script.js
+++ script.js
@@ -1,94 +1,74 @@
 const container = document.querySelector('#container')
 const resetButton = document.querySelector('#resetbtn')
 const toggleBorder = document.querySelector('#border-btn')
-
-
-
 let userSize = document.getElementById("numberInput").value
 console.log(userSize)
 
+let userColor = '';
+let rainbow = `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
-// Nest rowsAndColumns function inside an if statement (to better use buttons?)
 
-
+let drawingStyle;
 
 function rowsAndColumns(userSize) {
+
     let gridSizeCalc = (100 / userSize) + "%";
     console.log(gridSizeCalc)
     for (let b = 0; b < (userSize * userSize); b++) {
+
         const box = document.createElement('div')
-        // box = document.createElement('div')
         container.appendChild(box)
         box.setAttribute('style', `height:${gridSizeCalc};width:${gridSizeCalc}`)
-        
-        // Couldn't manage to get working properly, remnants of borders stayed. Scrapped
-        // toggleBorder.addEventListener('click', function () {
-        //     if (box.classList.contains('noborder')) {
-        //         box.removeAttribute('class', 'noborder')
-        //         box.setAttribute('class', 'border')
-        //         toggleBorder.innerHTML = 'Borders : ON'
-        //         reset()
-        //         console.log('border on')
 
-        //     } else {
-        //         box.removeAttribute('class', 'filled')
-        //         box.setAttribute('class', 'noborder')
-        //         toggleBorder.innerHTML = 'Borders : OFF'
-        //         console.log('border off')
-        //     }
-        // })
         resetButton.addEventListener('click', e => {
             box.removeAttribute('class', 'filled')
-
+            console.log('reset')
         })
 
-        box.addEventListener('mouseenter', e => {
+        function clickFill() {
             box.setAttribute('class', 'filled')
+            console.log('click')
+        }
+        function snakeFill() {
+            box.setAttribute('class', 'filled')
+            console.log('snake')
+
+        }
+        function clickPlacement() {
+            box.addEventListener('click', clickFill)
+        }
+        function snakePlacement() {
+            box.addEventListener('mouseenter', snakeFill)
+        }
+        document.querySelector('#drawing-style-snake').addEventListener('click', e => {
+            snakePlacement()
+            box.removeEventListener('click', clickFill)
 
         })
 
+        document.querySelector('#drawing-style-click').addEventListener('click', e => {
+            clickPlacement()
+            box.removeEventListener('mouseenter', snakeFill)
+        })
     }
 
 }
-rowsAndColumns(userSize)
 
-// resetButton.addEventListener('click', e => {
-//     box.removeAttribute('class', 'filled')
 
-// })
 
-// box.addEventListener('mouseenter', e => {
-//     box.setAttribute('class', 'filled')
 
-// })
-
-// Change grid size based on user input, deletes all then invoked rowsAndColumns
+// Changes grid size based on user input, deletes all then invokes rowsAndColumns
 document.getElementById("numberInput").addEventListener('change', e => {
     userSize = document.getElementById("numberInput").value
-    
     container.textContent = '';
-
     rowsAndColumns(userSize)
-    
 })
 
 
-// function reset() {
-//     resetButton.addEventListener('click', function () {
 
-//         box.removeAttribute('class', 'filled')
-        
-// })
-// }
 
-// function clickPlacement() {
-//     box.addEventListener('click', e => {
-//         box.setAttribute('class', 'filled')
-//     })
-// }
-// function snakePlacement() {
-//     box.addEventListener('mouseenter', e => {
-//         box.setAttribute('class', 'filled')
-//     })
-// }
 
+
+
+
+rowsAndColumns(16)
